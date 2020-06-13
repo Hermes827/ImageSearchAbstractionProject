331cc1be6916c147eb6ee1b1fdf62aa8100c0a5c
Chris Cable <ccable827@gmail.com>
2020-06-13 07:54:17 -0700

    fixed backend API issues

---
+++ recordApi/app/controllers/records_controller.rb
@@ -0,0 +1,17 @@
+class RecordsController < ApplicationController
+
+  def index
+     records = Record.all
+     render json: records, status: :ok
+   end
+
+   def create
+     record = Record.create!(record_params)
+     render json: record, status: :ok
+   end
+ ​
+   def record_params
+     params.require(:record).permit(:text)
+   end
+
+end
