c315277f065af867de79a5bd9fa74ab36fcab4f4
Chris Cable <ccable827@gmail.com>
2020-06-25 10:04:12 -0700

    building routes

---
+++ NJSbackend/app/routes/note_routes.js
@@ -0,0 +1,3 @@
+module.exports = function(app, db) {  app.get('/notes', (req, res) => {  res.send('Hello')  });};
+
+//in the walkthrough app.get was app.post and it didnt work until I changed it...
--- NJSbackend/server.js
+++ NJSbackend/server.js
@@ -3,5 +3,5 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
-
-app.listen(port, ()=> {console.log("we are live on " + port)})
+require('./app/routes')(app, {});
+app.listen(port, () => {  console.log('We are live on ' + port);});
